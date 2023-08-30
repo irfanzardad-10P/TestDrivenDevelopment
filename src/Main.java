4f91be2822eb0332bbeddb0875d370c09c34aa23
irfanzardad10P <irfan.zardad@10pearls.com>
2023-08-30 19:32:30 +0500

    lecture 15 integrating taxrate and interestrate

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		StockMarketYear account = new StockMarketYear(0, 0, 0, 0);
+		//StockMarketYear account = new StockMarketYear(0, 0, 0, 0);
 //		for (int i = 0; i < 60; i++) {
 //			System.out.println(i + " $: " + account.balance());
 //			account = account.nextYear();
---
+++ src/_DollarsTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class _DollarsTest {
+
+	@Test
+	public void addition() {
+		Dollars dollars = new Dollars(0);
+
+	}
+
+}
